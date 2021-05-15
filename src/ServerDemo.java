5029d03f8bab7bd01f45e0d6eff5dfbe8998caa3
HEO JIHUN <ys05143@gmail.com>
2021-05-15 19:55:33 +0900

    2020-05-16 / add some functions

--- src/ServerDemo.java
+++ src/ServerDemo.java
@@ -16,16 +16,56 @@ import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.StyledDocument;
-
+/*
 import kr.ac.konkuk.ccslab.cm.entity.CMMqttSession;
 import kr.ac.konkuk.ccslab.cm.info.CMConfigurationInfo;
 import kr.ac.konkuk.ccslab.cm.info.CMDBInfo;
 import kr.ac.konkuk.ccslab.cm.info.CMInfo;
+import kr.ac.konkuk.ccslab.cm.info.CMInteractionInfo;
 import kr.ac.konkuk.ccslab.cm.info.CMMqttInfo;
 import kr.ac.konkuk.ccslab.cm.manager.CMCommManager;
 import kr.ac.konkuk.ccslab.cm.manager.CMDBManager;
 import kr.ac.konkuk.ccslab.cm.manager.CMMqttManager;
 import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
+*/
+import kr.ac.konkuk.ccslab.cm.entity.CMGroup;
+import kr.ac.konkuk.ccslab.cm.entity.CMList;
+import kr.ac.konkuk.ccslab.cm.entity.CMMember;
+import kr.ac.konkuk.ccslab.cm.entity.CMMessage;
+import kr.ac.konkuk.ccslab.cm.entity.CMMqttSession;
+import kr.ac.konkuk.ccslab.cm.entity.CMRecvFileInfo;
+import kr.ac.konkuk.ccslab.cm.entity.CMSendFileInfo;
+import kr.ac.konkuk.ccslab.cm.entity.CMServer;
+import kr.ac.konkuk.ccslab.cm.entity.CMSession;
+import kr.ac.konkuk.ccslab.cm.entity.CMUser;
+import kr.ac.konkuk.ccslab.cm.event.CMBlockingEventQueue;
+import kr.ac.konkuk.ccslab.cm.event.CMDummyEvent;
+import kr.ac.konkuk.ccslab.cm.info.CMCommInfo;
+import kr.ac.konkuk.ccslab.cm.info.CMConfigurationInfo;
+import kr.ac.konkuk.ccslab.cm.info.CMDBInfo;
+import kr.ac.konkuk.ccslab.cm.info.CMFileTransferInfo;
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
+import kr.ac.konkuk.ccslab.cm.info.CMInteractionInfo;
+import kr.ac.konkuk.ccslab.cm.info.CMMqttInfo;
+import kr.ac.konkuk.ccslab.cm.manager.CMCommManager;
+import kr.ac.konkuk.ccslab.cm.manager.CMConfigurator;
+import kr.ac.konkuk.ccslab.cm.manager.CMDBManager;
+import kr.ac.konkuk.ccslab.cm.manager.CMMqttManager;
+import kr.ac.konkuk.ccslab.cm.sns.CMSNSUserAccessSimulator;
+import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+import javax.swing.JOptionPane;
 
 public class ServerDemo extends JFrame {
 
@@ -118,18 +158,41 @@ public class ServerDemo extends JFrame {
 					m_serverStub.terminateCM();
 					printMessage("Server Stopped!");
 				}
-				else if(InputString.equals("DBconf") || InputString.equals("2"))
+				
+				else if(InputString.equals("DBconf") || InputString.equals("2")) {
 					DBinformation();
+				}
+				
 				else if(InputString.equals("clear") || InputString.equals("3")) {
 					LogArea.setText(null);
 					serviceList();
 				}
-				else if(MenuString != null && MenuString.equals("/p"))
-				{
+				
+				else if(MenuString != null && MenuString.equals("/p")) {
 					if(MakePublish(targetString,InputString))
 						printMessage("successfully published msg to "+targetString + " " + InputString);
 					else
 						printMessage("Publish failed");
+				}
+				
+				else if(InputString.equals("ShowSession") || InputString.equals("5")) {
+					printSessionInfo();
+				}
+				
+				else if(InputString.equals("ShowGroup") || InputString.equals("6")) {
+					printGroupInfo();
+				}
+				
+				else if(InputString.equals("getUser") || InputString.equals("7")) {
+					printUserDB(queryGetUsers(0, -1, m_cmInfo));
+				}
+				
+				else if(InputString.equals("getGroup") || InputString.equals("8")) {
+					printGroupDB(queryGetGroup(0, -1, m_cmInfo));
+				}
+				
+				else if(InputString.equals("insertPublish") || InputString.equals("9")) {
+					makeGroup();
 				}
 				
 				else printMessage("---------WRONG COMMAND -------------");
@@ -166,6 +229,24 @@ public class ServerDemo extends JFrame {
 		}
 		
 		
+		
+	}
+public void serviceList() {
+		
+		// 서버에서 제공하는 서비스에 대해서 설명
+				printMessage("------------------------------");
+				printMessage("1. exit : Terminate CM");
+				printMessage("2. DBconf : check my DB conf");
+				printMessage("3. clear : clear log area");
+				printMessage("4. /p [sesssion/groupname] [msg] : publish");
+				printMessage("5. ShowSession : show session information");
+				printMessage("6. ShowGroup : show group information");
+				printMessage("7. getUser : select user DB");
+				printMessage("8. getGroup : select group DB");
+				printMessage("9. insertPublish : insert publish DB");
+				printMessage("------------------------------");
+			
+		
 	}
 	
 	public boolean MakePublish(String strTopic,String strMsg) {
@@ -192,7 +273,7 @@ public class ServerDemo extends JFrame {
 		return true ;
 	}
 	
-	public void DBinformation() {
+	public void DBinformation() { // DB access information
 	      CMInfo cmInfo=m_serverStub.getCMInfo();
 	      CMDBInfo dbInfo = cmInfo.getDBInfo();
 	      CMConfigurationInfo confInfo = cmInfo.getConfigurationInfo();
@@ -203,18 +284,186 @@ public class ServerDemo extends JFrame {
 	      printMessage( "user :"+ user );
 	      printMessage( "pass :"+ pass );
 	   }
-	public void serviceList() {
+	
+	public void printSessionInfo()
+	{
+		printMessage("----------------------------------------------------------------\n");
+		printMessage(String.format("%-20s%-20s%-10s%-10s%n", "session name", "session addr", "port", "#users"));
+		printMessage("----------------------------------------------------------------\n");
 		
-		// 서버에서 제공하는 서비스에 대해서 설명
-				printMessage("------------------------------");
-				printMessage("1. exit : Terminate CM");
-				printMessage("2. DBconf : check my DB conf");
-				printMessage("3. clear : clear log area");
-				printMessage("4. /p [sesssion/groupname] [msg] : publish");
-				printMessage("------------------------------");
-			
+		CMInteractionInfo interInfo = m_serverStub.getCMInfo().getInteractionInfo();
+		Iterator<CMSession> iter = interInfo.getSessionList().iterator();
+		while(iter.hasNext())
+		{
+			CMSession session = iter.next();
+			printMessage(String.format("%-20s%-20s%-10d%-10d%n", session.getSessionName(), session.getAddress()
+					, session.getPort(), session.getSessionUsers().getMemberNum()));
+		}
+		return;
+	}
+	
+	public void printGroupInfo()
+	{
+		String strSessionName = null;
+		
+		printMessage("====== print group information\n");
+		strSessionName = JOptionPane.showInputDialog("Session Name");
+		if(strSessionName == null)
+		{
+			return;
+		}
+		
+		CMInteractionInfo interInfo = m_serverStub.getCMInfo().getInteractionInfo();
+		CMSession session = interInfo.findSession(strSessionName);
+		if(session == null)
+		{
+			printMessage("Session("+strSessionName+") not found.\n");
+			return;
+		}
+		
+		printMessage("------------------------------------------------------------------\n");
+		printMessage(String.format("%-20s%-20s%-10s%-10s%n", "group name", "multicast addr", "port", "#users"));
+		printMessage("------------------------------------------------------------------\n");
+
+		Iterator<CMGroup> iter = session.getGroupList().iterator();
+		while(iter.hasNext())
+		{
+			CMGroup gInfo = iter.next();
+			printMessage(String.format("%-20s%-20s%-10d%-10d%n", gInfo.getGroupName(), gInfo.getGroupAddress()
+					, gInfo.getGroupPort(), gInfo.getGroupUsers().getMemberNum()));
+		}
+
+		printMessage("======\n");
+		return;
+	}
+	
+	public void makeGroup() { // make group 
+		//String sname= new String("session1"); //client msg 로 받아야함(요청한 client가 현재 속한 session)
+		//String saddr= new String("192.168.189.1");//client msg
+		//int sport= 7777;//client msg
+		//CMSession session =new CMSession("session1", "192.168.189.1", 7770);//client가 속한 session 객체
+		CMInteractionInfo interInfo = m_serverStub.getCMInfo().getInteractionInfo();
+		Iterator<CMSession> iter = interInfo.getSessionList().iterator();
+		CMSession session = iter.next();
+		
+		
+		int groupNum=session.getGroupList().size();//현재 group수
+	
+		
+		//새로운 group의 이름, 주소, port 번호  생성 (현재 group수로 만듦)
+		StringBuffer gnameformat= new StringBuffer("g1");
+		gnameformat.replace(1, 2, Integer.toString(groupNum+1));
+		String gname= new String(gnameformat.toString());
+		System.out.println(gname);
+		
+		StringBuffer gaddrformat= new StringBuffer("224.1.1.2");
+		gaddrformat.replace(6, 7, Integer.toString(groupNum+1));
+		String gaddr= new String(gaddrformat.toString());
+		System.out.println(gaddr);
+		
+		StringBuffer gportformat= new StringBuffer("7001");
+		gportformat.replace(2, 3, Integer.toString(groupNum));
+		String s_gport= new String(gportformat.toString());
+		int gport= Integer.parseInt(s_gport);
+		System.out.println(gport);
+		
+		//group 생성
+		session.createGroup(gname, gaddr, gport);
+		InsertGroup(gname, "heo", "noodle", "korean", "5000", "10000");
 		
 	}
+	
+	//if client made group, we can insert a row in group_table(DB)
+	public int InsertGroup(String group_id, String group_host, String restaurant, String res_category, String collected_amount, String least_price) {
+		CMInfo cmInfo=m_serverStub.getCMInfo();
+		String strQuery = "insert into group_table (group_id, group_host, restaurant, res_category, collected_amount, least_price) values ('" 
+				+group_id+"','"+group_host+"','"+restaurant+"','"+res_category+"','"+collected_amount+"','"+least_price+"');";
+		CMDBManager.init(cmInfo);
+		boolean a= CMDBManager.connectDB(cmInfo);
+		System.out.println("connect sucess: "+a);
+		int ret = CMDBManager.sendUpdateQuery(strQuery, cmInfo);
+
+		if(ret == -1)
+		{
+			System.out.println("makeOrderGroup(), error!");
+			return ret;
+		}
+
+		if(CMInfo._CM_DEBUG)
+			System.out.println("makeOrderGroup(), return value("+ret+").");
+
+		return ret;
+		
+	}
+	public static ResultSet queryGetUsers(int index, int num, CMInfo cmInfo)
+	{
+		String strQuery = null;
+		ResultSet rs = null;
+		if(index == 0 && num == -1)
+			strQuery = "select * from user_table;";
+		else
+			strQuery = "select * from user_table limit "+index+", "+num+";";
+
+		rs = CMDBManager.sendSelectQuery(strQuery, cmInfo);
+		
+		if(CMInfo._CM_DEBUG)
+			System.out.println("CMDBManager.queryGetUsers(), end.");
+
+		return rs;
+	}
+	public void printUserDB(ResultSet rs) {
+		try {
+			while(rs.next()) {
+				printMessage(rs.getInt(1) + "\t" + rs.getString(2)+ "\t" + rs.getString(3));
+			}
+			printMessage("-----------------------------------------------------\n");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static ResultSet queryGetGroup(int index, int num, CMInfo cmInfo) //select group_table (jihun makes group_table in mysql)
+	{
+		String strQuery = null;
+		ResultSet rs = null;
+		if(index == 0 && num == -1)
+			strQuery = "select * from group_table;";
+		else
+			strQuery = "select * from group_table limit "+index+", "+num+";";
+
+		rs = CMDBManager.sendSelectQuery(strQuery, cmInfo);
+		
+		if(CMInfo._CM_DEBUG)
+			System.out.println("CMDBManager.queryGetUsers(), end.");
+
+		return rs;
+	}
+	public void printGroupDB(ResultSet rs) { //print about group_table in mysql
+		try {
+			while(rs.next()) {
+				printMessage(rs.getString(1) + "\t" + rs.getString(2)+ "\t" + rs.getString(3) + "\t" + rs.getString(4) + "\t" + rs.getString(5)+ "\t" + rs.getString(6));
+			}
+			printMessage("-----------------------------------------------------\n");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 	
 	//---------------------------------------서버 로그에 프린트 ----------------------------------
 	public void printMessage(String strText)
@@ -245,6 +494,20 @@ public class ServerDemo extends JFrame {
 		
 		return;
 	}
+	
+	///////////////not finish!!!!!!!!!!!!!////////////////////////
+	public void removeGroup(String targetGroup) {
+		CMInteractionInfo interInfo = m_serverStub.getCMInfo().getInteractionInfo();
+		Iterator<CMSession> iter = interInfo.getSessionList().iterator();
+		CMSession session = iter.next();
+		
+		
+		
+		session.removeGroup(targetGroup);
+		
+		
+	}
+	///////////////////////////////////////////////
 	//-------------------------------------------------------------------------------------
 	
 
